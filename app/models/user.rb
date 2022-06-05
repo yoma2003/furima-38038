b5c187d72ddb7f702fe97bd599d997d5c5d0259b
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-06 01:03:16 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :kana_last_name,  presence: true
+  validates :kana_first_name, presence: true
+  validates :date_of_birth,   presence: true
 end
