3520bbf3f724601976201a134cf1e39b6d044755
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-06 19:12:45 +0900

    ユーザーモデルバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User < ApplicationRecord
 
   validates :nickname,        presence: true
   validates :last_name,       presence: true
+  validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, allow_blank: true
   validates :first_name,      presence: true
+  validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, allow_blank: true
   validates :kana_last_name,  presence: true
+  validates :kana_last_name,  format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
   validates :kana_first_name, presence: true
+  validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
   validates :date_of_birth,   presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
 end
