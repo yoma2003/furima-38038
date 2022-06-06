4aa5c0153d6c72b1d1f7fc18929c412eafe69ce2
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-06 15:51:58 +0900

    FactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    gimei = Gimei.name
+    nickname              {Faker::Name.last_name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {gimei.last.kanji}
+    first_name            {gimei.first.kanji}
+    kana_last_name        {gimei.last.katakana}
+    kana_first_name       {gimei.first.katakana}
+    date_of_birth         {Faker::Date.between(from: '1980-01-01', to: '2000-01-01')}
   end
 end
