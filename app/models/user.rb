9236b48a35d91bc8f262a6ba4c05e28708f19d35
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-06 18:33:00 +0900

    パスワード正規表現バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :kana_last_name,  presence: true
   validates :kana_first_name, presence: true
   validates :date_of_birth,   presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
 end
