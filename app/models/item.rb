4e32f0c81a78a82ea61f51a902fca851ec2602fd
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-07 14:47:46 +0900

    ActiveHashのアソシエーション・バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefectures
+  belongs_to :day_to_ship
+
+  validates :category_id, :condition, :condition, :delivery_charge, :prefectures, :day_to_ship, numericality: { other_than: 1, message: "can't be blank" } 
 end
