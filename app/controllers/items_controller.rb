00cf86d946415c1d3c63e7984c6a30c455618182
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-07 19:01:57 +0900

    itemsコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :describe, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
