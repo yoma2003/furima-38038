3ee54f1a0edccfe85900604aaba3ff770763a4ae
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-07 15:19:59 +0900

    ActiveHashモデル名修正

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -1,4 +1,4 @@
-class DayToShip < ActiveHash::Base
+class DaysToShip < ActiveHash::Base
   self.data = [
     {id: 1, name: '---'},
     {id: 2, name: '1~2日で発送'},
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :prefectures
-  belongs_to :day_to_ship
+  belongs_to :days_to_ship
 
   has_one_attached :image
 
-  validates :category_id, :condition, :condition, :delivery_charge, :prefectures, :day_to_ship, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :category_id, :condition, :condition, :delivery_charge, :prefectures, :days_to_ship, numericality: { other_than: 1, message: "can't be blank" } 
 end
