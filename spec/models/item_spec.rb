0cced2a9f3367b593c064de518e3f971b6260011
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-08 14:34:56 +0900

    itemモデル単体テスト編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe Item, type: :model do
       end
     end
     context '新規登録できない場合' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it 'nameが空では登録できない' do
         @item.name = ''
         @item.valid?
@@ -66,6 +71,11 @@ RSpec.describe Item, type: :model do
         @item.price = '５００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
+      end      
+      it 'userが紐付いていなければ登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
