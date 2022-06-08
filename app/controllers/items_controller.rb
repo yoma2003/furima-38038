5e97f23e5d7b6b483758448bc1fe967d1093995f
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-08 18:24:09 +0900

    ログイン制御修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
