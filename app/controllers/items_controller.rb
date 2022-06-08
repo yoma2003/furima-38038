420aaa93014287b04acf80fb2a5b082ee3e8d8f8
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-08 19:29:51 +0900

    アクセス制御の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :updatem, :destroy]
 
   def index
     @items = Item.order('created_at DESC').includes(:user)
