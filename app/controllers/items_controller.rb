a3a4be9c3a5e2318293d9eb2b7d7c90677d1ea8d
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-08 18:12:53 +0900

    indexアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: [:edit, :update]
 
   def index
-    @items = Item.order('created_at DESC')
+    @items = Item.order('created_at DESC').includes(:user)
   end
 
   def new
