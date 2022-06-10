a806ad8915dce1c85e116bbfb35c5cad831acb48
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-10 23:01:24 +0900

    createアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, except: [:index] do
-    resources :orders, only: [:index]
+    resources :orders, only: [:index, :create]
   end
 end
