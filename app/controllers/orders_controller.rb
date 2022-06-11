fe17041d99c19d4db7952a5b4a07d1729becb214
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-11 11:24:31 +0900

    Formオブジェクト編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,17 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id
 
   def save
-    
+    order = Order.create(user_id: user_id,item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id,municipality: municipality, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
   end
 end
