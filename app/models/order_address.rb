45d182fd966f75734747728c3e1da641fa84b07a
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-13 00:03:42 +0900

    Formオブジェクトバリデーション追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
@@ -10,6 +10,7 @@ class OrderAddress
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
