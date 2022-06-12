bf3ad95373b3427ebecf950239552fd3ae847fe2
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-13 00:03:20 +0900

    createアクション編集（決済処理の追加）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,17 +5,25 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      Payjp.api_key = "sk_test_4d55c03a8be7c9adc0a7ac72"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
+  private
+
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 end
