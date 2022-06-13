516c0ca11cc284292ce2ef7f6029a81bf42787f8
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-13 11:56:43 +0900

    環境変数にAPI鍵を定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = "sk_test_4d55c03a8be7c9adc0a7ac72"
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,
         card: order_params[:token],
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_23dbcd8a8b951d883f1c8624");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
