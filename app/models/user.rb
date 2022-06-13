19f17fa3004f5d51d363c632572f083a6f61b833
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-13 11:25:49 +0900

    userモデルリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,12 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :kana_last_name
-    validates :kana_first_name
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, allow_blank: true }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, allow_blank: true }
+    validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
     validates :date_of_birth
   end
-
-  validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, allow_blank: true
-  validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, allow_blank: true
-  validates :kana_last_name,  format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
-  validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
