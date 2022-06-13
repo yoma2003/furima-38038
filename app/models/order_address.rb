17ec64310a51184d07b32b34bf6e8a6c3ab2b871
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-13 11:21:51 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,14 +3,14 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :token
 
   with_options presence: true do
-    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :token
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, allow_blank: true }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank", allow_blank: true }
     validates :municipality
     validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, allow_blank: true }
     validates :user_id
     validates :item_id
-    validates :token
   end
 
   def save
